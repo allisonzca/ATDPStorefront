3895fb48b3a1e6b1315ad4479e6e49ac4472f6aa
timothyyyyy <30322907+timothyyyyy@users.noreply.github.com>
2017-07-20 11:22:34 -0700

    Update purchaseForm.php
    I added basically everything? Feel free to change the variable names if needed.

--- purchaseForm.php
+++ purchaseForm.php
@@ -11,10 +11,26 @@ echo printHead();
 ?>
 
 <form method="post" action="success.php"> 
-<!-- 
-personal info, address, credit card number, shipping,
-use functions cartCalc, printCart
--->
+	<!--Address Stuff-->
+	<h3>Address: </h3>
+		<input type="number" name="address" class="sBoxes" placeholder="Address" required>
+		<input type="text" name="street" class="sBoxes" placeholder="Street" required>
+		<input type="text" name="city" class="sBoxes" placeholder="City" required>
+		<select name="state" class="sBoxes" required>
+			<option label=" "></option>
+		<?php
+			$state = array("AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MH", "MA", "MI", "FM", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "MP", "OH", "OK", "OR", "PW", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "VI", "WA", "WV", "WI", "WY");
+			for ($sint = 0; $sint < count($state); $sint++){
+			echo"<option value=\"$state[$sint]\">$state[$sint]</option>";}
+		?>
+		</select>
+		<input type="number" name="zip" class="sBoxes" placeholder="Zip Code" required>
+	</div>
+	<!--Credit Card-->
+	<h3>Credit Card: <h3>
+		<input type="number" name="credit" placeholder ="Credit Card Number" maxlength="16" required>
+	<!--Confirm Purchase-->
+		<input type="submit" name="button" value="Confirm Purchase" id="submit">
 </form>
 
 <?php 
