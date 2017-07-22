a27aedbbb91674252fc047f97da10dc356aa04bf
allisonzca <30322873+allisonzca@users.noreply.github.com>
2017-07-21 20:07:49 -0700

    Update itemList.sql

--- itemList.sql
+++ itemList.sql
@@ -10,52 +10,51 @@ purchase | idk mayb if youd like to track all the purchases made,, put total pri
 */
 
 CREATE TABLE IF NOT EXISTS `items` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `name` varchar NOT NULL,
-  `image` varchar NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `image` text NOT NULL,
   `price` decimal(4,2) NOT NULL,
-  `desc` varchar NOT NULL,
-  `type` varchar NOT NULL,
-  PRIMARY KEY (`ID`)
+  `desc` text NOT NULL,
+  `type` text NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
 INSERT INTO `items` 
 	(`name`, `image`, `price`, `desc`, `type`)
 VALUES
-	('anxieTee', 'images/handsTee.png', '10.99', 'starter pack', 'help'),
+	('anxieTee', 'images/anxietee.png', '10.99', 'starter pack', 'help'),
 	('Catch these hands Tee', 'images/handsTee.png', '9.99', 'sum handz', 'help'),
-	('Flee Tee', 'images/handsTee.png', '9.99', 'flee on ur tee', 'help'),
-	('Pupper Tee', 'images/handsTee.png', '2.99', 'precious floof on ur abdomen', 'wholesome'),
+	('Flee Tee', 'fleetee.png', '9.99', 'flee on ur tee', 'help'),
+	('Pupper Tee', 'images/puppertee.png', '2.99', 'precious floof on ur abdomen', 'wholesome'),
 	('Eggo Tee', 'images/handsTee.png', '2.99', 'now u never hav 2 leggo ur eggo', 'wholesome'),
 	('Birb Tee', 'images/handsTee.png', '10.99', 'singz sweeter than u', 'wholesome'),
-	('Acorn tee', 'images/handsTee.png', '9.99', 'nut', 'collegeboard')
-	('Good score Tee', 'images/handsTee.png', '19.99', 'is only a 5', 'collegeboard');
-	('Josefin Slab Tee', 'images/handsTee.png', '24.99', 'collegeboard\'s fave font', 'collegeboard'); 
-
-
+	('Acorn tee', 'images/handsTee.png', '9.99', 'nut', 'collegeboard'),
+	('Good score Tee', 'images/handsTee.png', '19.99', 'is only a 5', 'collegeboard'),
+	('Josefin Slab Tee', 'images/handsTee.png', '24.99', 'collegeboard\'s fave font', 'collegeboard');
 
 CREATE TABLE IF NOT EXISTS `customers` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `name` varchar NOT NULL,
-  `address` varchar NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `address` text NOT NULL,
   `phone` int NOT NULL,
-  `email` varchar NOT NULL,
-  PRIMARY KEY (`ID`)
+  `email` text NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `purchases` (
-	`ID` int NOT NULL AUTO_INCREMENT, 
-	`customer_id` varchar,
+	`id` int NOT NULL AUTO_INCREMENT, 
+	`customer_id` int,
 	`item_id` int,
-	`price_id` decimal(100,2),
-	PRIMARY KEY (`ID`)
+	`price` decimal(10,2),
+	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `cart` (
-	`ID` int NOT NULL AUTO_INCREMENT, 
-	`customer_id` varchar,
+	`id` int NOT NULL AUTO_INCREMENT, 
+	`customer_id` int,
 	`item_id` int,
-	PRIMARY KEY (`ID`)
+	PRIMARY KEY (`id`)
 );
+
 
 
